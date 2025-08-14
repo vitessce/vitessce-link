0874e001e14bfa0c604cfb04385a2e868d13c7f7
Tabassum Kakar <tkakar@wpi.edu>
2025-08-14 17:09:22 -0400

    Added examples

---
+++ src/pages/Examples.tsx
@@ -0,0 +1,10 @@
+// import { Examples } from "../components/Example";
+import Layout from "@theme/Layout";
+import {ExampleList} from "../components/ExampleList/ExampleList";
+export default function Home() {
+	return (
+		<Layout>
+			<ExampleList/>
+		</Layout>
+	);
+}
